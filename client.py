b761a74772a9319cff77485c5a09e37ed2d16c50
EnterSnowman <kuza1812@mail.ru>
2018-12-17 17:59:29 +0200

    Added last files

---
+++ client.py
@@ -0,0 +1,43 @@
+import asyncio
+import json
+from collections import namedtuple, deque
+
+
+async def tcp_echo_client(loop, msg_count):
+    memory = deque(maxlen=100)
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8888,
+                                                   loop=loop)
+    hello_message = "Server, " + str(msg_count) + " hi!"
+    print('Send: %r' % hello_message)
+    writer.write(hello_message.encode())
+
+    is_open = True
+    counter = 0
+    try:
+        while is_open and counter < msg_count:
+            data = await reader.read(512)
+            if data:
+                counter += 1
+                # print(data.decode())
+                x = json.loads(data, object_hook=lambda d: namedtuple('msg', d.keys())(*d.values()))
+                memory.append(x)
+                # writer.write(str(counter).encode())
+            else:
+                is_open = False
+
+    except KeyboardInterrupt:
+        pass
+    writer.write('quit'.encode())
+    writer.close()
+    for m in memory:
+        print(m)
+
+    print('Close the socket')
+
+
+# message = 'Sashka, hi'
+loop = asyncio.get_event_loop()
+msg_counts = [300, 500, 700]
+tasks = [tcp_echo_client(loop, m) for m in msg_counts]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
---
+++ server.py
@@ -0,0 +1,63 @@
+import asyncio
+import json
+import time
+import random
+
+
+def generate_json():
+    data = {"timestamp": time.time()}
+    for i in range(1, 11):
+        data["float_" + str(i)] = random.random()
+        data["int_" + str(i)] = random.randint(0, 1)
+    return json.dumps(data)
+
+
+class Server:
+    __current_data = None
+    __period = 1 / 30.
+    __writer = None
+    writers = []
+
+    async def generate_data(self):
+        while True:
+            await asyncio.sleep(self.__period)
+            self.__current_data = generate_json()
+            if len(self.writers) > 0:
+                for w in self.writers:
+                    w.write(self.__current_data.encode())
+                    await w.drain()
+            # if self.__writer is not None:
+            #     if not self.__writer.is_closing():
+            #         self.__writer.write(self.__current_data.encode())
+            #         await self.__writer.drain()
+
+    async def handle_client(self, reader, writer):
+        data = await reader.read(100)
+        message = data.decode()
+        addr = writer.get_extra_info('peername')
+        print("Received %r from %r" % (message, addr))
+
+        self.writers.append(writer)
+        self.__writer = writer
+        request = None
+        while request != 'quit':
+            request = await reader.read(100)
+            if len(request.decode()) > 0:
+                print("Request from", addr, request.decode())
+                request = request.decode()
+        self.__writer = None
+        self.writers.remove(writer)
+        writer.close()
+        print("Connection with", addr, "closed")
+
+    def run(self):
+        loop = asyncio.get_event_loop()
+        coro = asyncio.start_server(self.handle_client, '127.0.0.1', 8888, loop=loop)
+        print("Server started")
+        loop.run_until_complete(asyncio.gather(coro, self.generate_data()))
+        loop.close()
+
+
+if __name__ == "__main__":
+    server = Server()
+    server.run()
